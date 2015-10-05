b392a93d55c45a9c26b7a0567ef3779365629406
Bradford Smith <bsmith8@stevens.edu>
2015-10-05 13:16:55 -0400

    Finished Lab 4

---
+++ Lab4/stack_overflow.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+static char mybuf[4096];
+
+void unbelievable()
+{
+	printf("I don't believe it!\n");
+	exit(0);
+}
+
+void echo_input(char* arg)
+{
+	char buf[256];
+
+	strcpy(buf, arg);
+	printf("echo: %s\n", buf);
+}
+
+void read_input(FILE *fp, char *buf, size_t len)
+{
+	if (fread(buf, len, 1, fp) < 0) {
+		perror("error reading input");
+		exit (1);
+	}
+}
+
+int main(int argc, char* argv[]) 
+{
+	read_input(stdin, mybuf, sizeof(mybuf));
+
+	echo_input(mybuf);
+
+	return 0;
+}
