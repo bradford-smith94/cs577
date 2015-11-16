02ace9fbb89884074683437324f52fbbe0a3ad33
Bradford Smith <bsmith8@stevens.edu>
2015-11-15 19:12:49 -0500

    Started Lab 8

---
+++ Lab8/include/sniffer.h
@@ -0,0 +1,27 @@
+/* Bradford Smith (bsmith8)
+ * CS 577 Lab 8 sniffer.h
+ * 11/12/2015
+ * "I pledge my honor that I have abided by the Stevens Honor System."
+ */
+
+#ifndef _SNIFFER_H_
+#define _SNIFFER_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <pcap.h>
+
+/* This is the header for the pcap sniffer program for Lab 8 */
+
+int main(int, char**);
+
+/* Libpcap functions
+ * pcap_open_offline()
+ * pcap_loop()
+ * pcap_next()
+ * pcap_create()
+ */
+
+#endif /* _SNIFFER_H_ */
---
+++ Lab8/src/main.c
@@ -0,0 +1,16 @@
+/* Bradford Smith (bsmith8)
+ * CS 577 Lab 8 main.c
+ * 11/12/2015
+ * "I pledge my honor that I have abided by the Stevens Honor System."
+ */
+
+#include "sniffer.h"
+
+/* pre: takes in int argc and char** argv command line args
+ * post: runs the sniffer program
+ * return: 0 on success something else on failure
+ */
+int main(int argc, char** argv)
+{
+    return 0;
+}
