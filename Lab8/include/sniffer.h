1a2ffd02835861ebf323d73667f9bcd74b8f99b2
Bradford Smith <bsmith8@stevens.edu>
2015-11-18 00:02:39 -0500

    Progress
    Did some research, added a useful header, and started laying out the basic
    structure of the program.

--- Lab8/include/sniffer.h
+++ Lab8/include/sniffer.h
@@ -1,6 +1,6 @@
 /* Bradford Smith (bsmith8)
  * CS 577 Lab 8 sniffer.h
- * 11/12/2015
+ * 11/17/2015
  * "I pledge my honor that I have abided by the Stevens Honor System."
  */
 
@@ -11,7 +11,18 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+
+#include <time.h>
+
 #include <pcap.h>
+#include <arpa/inet.h>
+#include <netinet/ip.h>
+#include <linux/if_ether.h> /* provides constants for common EtherTypes */
+/* EtherType Constants include:
+ * ETH_P_IP (0x0800) for ethernet IP
+ * ETH_P_ARP (0x0806) for ARP
+ * ETH_P_8021Q (0x8100) for VLAN tags
+ */
 
 /* This is the header for the pcap sniffer program for Lab 8 */
 
--- Lab8/src/main.c
+++ Lab8/src/main.c
@@ -1,6 +1,6 @@
 /* Bradford Smith (bsmith8)
  * CS 577 Lab 8 main.c
- * 11/12/2015
+ * 11/17/2015
  * "I pledge my honor that I have abided by the Stevens Honor System."
  */
 
@@ -12,5 +12,55 @@
  */
 int main(int argc, char** argv)
 {
+    unsigned int ip_count; /* count of IP packets */
+    unsigned int tcp_count; /* count of TCP packets */
+    unsigned int udp_count; /* count of UDP packets */
+
+    clock_t start; /* time started */
+    clock_t end; /* time ended */
+    double dtime; /* delta time */
+
+    struct pcap_pkthdr header; /* packet header */
+    const u_char *packet; /* full packet */
+
+    pcap_t* pcapfd;
+    char errbuf[PCAP_ERRBUF_SIZE];
+
+    if (argc < 2)
+    {
+        printf("usage: %s <pcap file>\n", argv[0]);
+        fflush(stdout);
+        exit(1);
+    }
+    else if (argc > 2)
+    {
+        /* need to check for commands */
+        printf("usage: %s <pcap file>\n", argv[0]);
+        printf("more advanced functionality not implemented yet\n");
+        fflush(stdout);
+        exit(1);
+    }
+
+    if ((pcapfd = pcap_open_offline(argv[1], errbuf)) == NULL)
+    {
+        printf("Cannot open pcap file!\n");
+        fflush(stdout);
+        exit(1);
+    }
+
+    /* start timed code here */
+    start = clock();
+
+    /* loop over all the packets */
+    while (packet = pcap_next(pcapfd, &header))
+    {
+    }
+
+    /* end timed code here */
+    end = clock();
+
+    /* calculate time difference */
+    dtime = (double)(end - start) / CLOCKS_PER_SEC;
+
     return 0;
 }
