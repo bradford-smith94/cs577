eb4680b35c1760fff9e21b68e949546647231ba6
Bradford Smith <bsmith8@stevens.edu>
2015-12-01 12:11:32 -0500

    Did part 1 of 7

---
+++ Lab9/include/notavirus.h
@@ -0,0 +1,15 @@
+/* Bradford Smith (bsmith8)
+ * CS 577 Lab 9 notavirus.h
+ * 12/01/2015
+ * "I pledge my honor that I have abided by the Stevens Honor System."
+ */
+
+#ifndef _NOTAVIRUS_H_
+#define _NOTAVIRUS_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+#endif /* _NOTAVIRUS_H_ */
